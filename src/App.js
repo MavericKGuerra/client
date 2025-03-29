cdbe9012842bfe7636329c8f93e9b9c9e89fa86c
Eduar <eduarherrera0507@gmail.com>
2025-03-29 18:04:01 -0500

    Se agrega alertas amigables al usuario junto con las funciones de eliminar y actualizar empleados

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Swal from "sweetalert2";
 
 function App() {
   const [nombre, setNombre] = useState("");
-  const [edad, setEdad] = useState(0);
+  const [edad, setEdad] = useState();
   const [pais, setPais] = useState("");
   const [cargo, setCargo] = useState("");
-  const [anios, setAnios] = useState("");
+  const [anios, setAnios] = useState();
+  const [id, setId] = useState();
+
+  const [editar, setEditar] = useState(false);
 
   const [empleadosList, setEmpleados] = useState([]);
 
@@ -23,6 +27,7 @@ function App() {
 
   useEffect(getEmpleados, []);
 
+  //llamado para guardar la información
   const add = () => {
     Axios.post("http://localhost:3001/create", {
       nombre: nombre,
@@ -32,8 +37,74 @@ function App() {
       anios: anios,
     }).then(() => {
       getEmpleados();
-      setNombre("");
+      limpiarCampos();
+      Swal.fire({
+        title: "<strong>Registro Exitoso</strong>",
+        html:
+          "<i>El empleado <strong>" +
+          nombre +
+          "</strong> ¡ha sido registrado!</i>",
+        icon: "success",
+        timer: 3000,
+      });
     });
+  };
+
+  const update = () => {
+    Axios.put("http://localhost:3001/update", {
+      id: id,
+      nombre: nombre,
+      edad: edad,
+      pais: pais,
+      cargo: cargo,
+      anios: anios,
+    }).then(() => {
+      getEmpleados();
+      limpiarCampos();
+      Swal.fire({
+        title: "<strong>Actualización Exitosa</strong>",
+        html:
+          "<i>El empleado <strong>" +
+          nombre +
+          "</strong> ¡ha sido actualizado!</i>",
+        icon: "success",
+      });
+    });
+  };
+
+  //Llamado a función eliminar
+  const deleteEmpleado = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then(() => {
+      getEmpleados();
+      limpiarCampos();
+      Swal.fire({
+        title: "<strong>Eliminación Exitosa</strong>",
+        html:
+          "<i>El empleado <strong>" +
+          nombre +
+          "</strong> ¡ha sido Eliminado!</i>",
+        icon: "success",
+      });
+    });
+  };
+
+  const limpiarCampos = () => {
+    setNombre("");
+    setEdad("");
+    setPais("");
+    setCargo("");
+    setAnios("");
+    setEditar(false); //Se llama false para refrescar el boton "registrarme"
+  };
+
+  const editarEmpleado = (val) => {
+    setEditar(true);
+    setNombre(val.nombre);
+    setEdad(val.edad);
+    setPais(val.pais);
+    setCargo(val.cargo);
+    setAnios(val.anios);
+    setId(val.id);
   };
 
   return (
@@ -51,6 +122,7 @@ function App() {
                 setNombre(event.target.value);
               }}
               className="form-control"
+              value={nombre}
               placeholder="Ingrese su nombre"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -66,6 +138,7 @@ function App() {
                 setEdad(event.target.value);
               }}
               className="form-control"
+              value={edad}
               placeholder="Ingrese su edad"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -81,6 +154,7 @@ function App() {
                 setPais(event.target.value);
               }}
               className="form-control"
+              value={pais}
               placeholder="Ingrese su país de nacimiento"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -96,6 +170,7 @@ function App() {
                 setCargo(event.target.value);
               }}
               className="form-control"
+              value={cargo}
               placeholder="Ingrese su cargo actual"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -111,6 +186,7 @@ function App() {
                 setAnios(event.target.value);
               }}
               className="form-control"
+              value={anios}
               placeholder="Ingrese los años de experiencia"
               aria-label="Username"
               aria-describedby="basic-addon1"
@@ -118,9 +194,20 @@ function App() {
           </div>
         </div>
         <div className="card-footer text-muted">
-          <button className="btn btn-success" onClick={add}>
-            Registrarme
-          </button>
+          {editar ? (
+            <div>
+              <button className="btn btn-warning m-2" onClick={update}>
+                Actualizar
+              </button>
+              <button className="btn btn-danger m-2" onClick={limpiarCampos}>
+                Cancelar
+              </button>
+            </div>
+          ) : (
+            <button className="btn btn-success" onClick={add}>
+              Registrarme
+            </button>
+          )}
         </div>
       </div>
 
@@ -132,17 +219,44 @@ function App() {
             <th scope="col">Pais</th>
             <th scope="col">Cargo</th>
             <th scope="col">Años Experiencia</th>
+            <th scope="col">Acciones</th>
           </tr>
         </thead>
         <tbody>
           {empleadosList.map((val, key) => {
             return (
-              <tr key={key}>
+              <tr key={val.id}>
                 <td>{val.nombre}</td>
                 <td>{val.edad}</td>
                 <td>{val.pais}</td>
                 <td>{val.cargo}</td>
                 <td>{val.anios}</td>
+                <td>
+                  <div
+                    className="btn-group"
+                    role="group"
+                    aria-label="Basic example"
+                  >
+                    <button
+                      type="button"
+                      onClick={() => {
+                        editarEmpleado(val);
+                      }}
+                      className="btn btn-info"
+                    >
+                      Editar
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        deleteEmpleado(val.id);
+                      }}
+                      className="btn btn-danger"
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                </td>
               </tr>
             ); //el boton lo que hace es traer la información de la BS
           })}
